2b02668cc81c89ca7681ebfefbfb42102f59cdef
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-10 16:21:09 +0200

    fix(Dwarf.cs): fix typo

--- WarriorsDuel/Classes/Dwarf.cs
+++ WarriorsDuel/Classes/Dwarf.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WarriorsDuel.Classes
 {
-    internal class Dwarf
+    internal class Dwarf : Warrior
     {
+        private int _armor;
+
+        public int Armor { get => _armor; set => _armor = value; }
+        public Dwarf(String name, int pv, int nbAttacks, int armor) : base (name, pv, nbAttacks)
+        {
+            Armor = armor;
+        }
     }
 }
