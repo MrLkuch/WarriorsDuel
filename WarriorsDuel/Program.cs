16051584c4f8913251151f1f1dd7bb3240f7e829
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-10 15:10:56 +0200

    feat(Program.cs): create Combat function

--- WarriorsDuel/Program.cs
+++ WarriorsDuel/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WarriorsDuel.Classes;
+
+void Combat(Warrior warriorOne, Warrior warriorTwo)
+{
+    int turn = 1;
+
+    while (warriorOne.Pv > 0 && warriorTwo.Pv > 0)
+    {
+        Console.WriteLine($"Turn : {turn}");
+
+        if (turn%2 == 0)
+        {
+            for (int i = 0; i < warriorTwo.NbAttacks; i++)
+            {
+                
+
+                if (warriorOne.Pv > 0)
+                {
+                    int dmg = new Random().Next(1, 7);
+
+                    warriorOne.Pv -= dmg;
+                    if (warriorOne.Pv < 0)
+                    {
+                        warriorOne.Pv = 0;
+                    }
+
+                    Console.WriteLine($"{warriorTwo.Name} inflicts {dmg} !");
+                    Console.WriteLine($"{warriorOne.Name} have {warriorOne.Pv} left. \n");
+                }
+                
+            }
+        }
+        else
+        {
+
+            for (int i = 0; i < warriorOne.NbAttacks; i++)
+            {
+               
+
+                if(warriorTwo.Pv > 0)
+                {
+                    int dmg = new Random().Next(1, 7);
+                    warriorTwo.Pv -= dmg;
+                    if (warriorTwo.Pv < 0)
+                    {
+                        warriorTwo.Pv = 0;
+                    }
+                    
+
+                    Console.WriteLine($"{warriorOne.Name} inflicts {dmg} !");
+                    Console.WriteLine($"{warriorTwo.Name} have {warriorTwo.Pv} left. \n");
+                }
+               
+            }
+        }
+
+        turn++;
+    }
+
+    if( warriorOne.Pv > 0 )
+    {
+        Console.WriteLine($"{warriorTwo.Name} is vanquished ! \n");
+        Console.WriteLine($"{warriorOne.Name} Win !");
+    }
+    else
+    {
+        Console.WriteLine($"{warriorOne.Name} is vanquished ! \n");
+        Console.WriteLine($"{warriorTwo.Name} Win !");
+    }
+
+}
+
+Warrior friedrich = new Warrior("Friedrich", 10, 2);
+Warrior sigismund = new Warrior("Sigismund", 15, 1);
+
+Combat(friedrich, sigismund);
