b2fd9ca6dbbb0046ae720f91039754778cf13ccc
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-10 16:23:36 +0200

    feat(Elf.cs): create Elf class

---
+++ WarriorsDuel/Classes/Elf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarriorsDuel.Classes
+{
+    internal class Elf : Warrior
+    {
+        public Elf(String name, int pv, int nbAttacks, int armor) : base (name, pv, nbAttacks)
+        {
+
+        }
+    }
+}
