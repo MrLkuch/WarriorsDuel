be0b54b94f9286efb92221261f3bd8f5d17e8f88
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-11 09:53:01 +0200

    feat(Warrior.cs): add armor attribute, dmg ^& receivedDmg function

--- WarriorsDuel/Classes/Warrior.cs
+++ WarriorsDuel/Classes/Warrior.cs
@@ -9,18 +9,37 @@ namespace WarriorsDuel.Classes
     internal class Warrior
     {
         private String _name;
-        private int _pv;
+        private int _hp;
         private int _nbAttacks;
+        private int _armor;
 
         public String Name { get => _name; set => _name = value; }
-        public int Pv { get => _pv; set => _pv = value; }
+        public int Hp { get => _hp; set => _hp = value; }
         public int NbAttacks { get => _nbAttacks; set => _nbAttacks = value; }
+        public int Armor { get => _armor; set => _armor = value; }
 
-        public Warrior(String name, int pv, int nbAttacks)
+        public Warrior(String name, int hp, int nbAttacks, int armor = 0)
         {
             Name = name;
-            Pv = pv;
+            Hp = hp;
             NbAttacks = nbAttacks;
+            Armor = armor;
+        }
+
+        public virtual int Attack()
+        {
+            int dmg = new Random().Next(1, 7); ;
+            return dmg;
+        }
+
+        public int ReceiveDmg(int dmg)
+        {
+            int receivedDmg = 0;
+            if (dmg >= Armor)
+            {
+                receivedDmg += dmg - Armor;
+            }
+            return receivedDmg;
         }
     }
 }
